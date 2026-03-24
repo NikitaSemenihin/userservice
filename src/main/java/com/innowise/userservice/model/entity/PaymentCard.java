ec699345279d49a17f692e6d2e611db79354ceee
Nikita <semenihin.rink@gmail.com>
2026-03-25 00:01:48 +0300

    USER-0: remove unused import

--- src/main/java/com/innowise/userservice/model/entity/PaymentCard.java
+++ src/main/java/com/innowise/userservice/model/entity/PaymentCard.java
@@ -1,6 +1,5 @@
 package com.innowise.userservice.model.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
